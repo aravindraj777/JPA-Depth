5f8d0ad96029544ed8d422a094e782fbb3bb0ccc
Aravind <aravindmunna240@gmail.com>
2024-11-24 12:21:09 +0530

    first commit

---
+++ src/main/java/com/buildlive/jpaDepth/JpaDepthApplication.java
@@ -0,0 +1,13 @@
+package com.buildlive.jpaDepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDepthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildlive/jpaDepth/JpaDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildlive.jpaDepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
